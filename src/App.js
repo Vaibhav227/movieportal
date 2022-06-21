66ca1438bfc3f8e333096c7f333fefc88446dea0
Vaibhav Sharma <51129176+Vaibhav227@users.noreply.github.com>
2022-06-21 11:31:27 +0530

    files uploaded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Homepage from './pages/Homepage';
+
+const App = () => {
+  return (
+    <>
+      <Homepage />
+    </>
+  );
+};
+
+export default App;
